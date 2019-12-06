d0e6b87e291fc9535324830b2b0c93a89b1f62a5
mobapp07 <mobapp07@student.ehb.be>
2019-12-06 11:44:45 +0100

    Initial commit Added all entities and DAO

---
+++ src/main/java/be/ehb/products/model/Product.java
@@ -0,0 +1,50 @@
+package be.ehb.products.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.DecimalMin;
+import java.math.BigDecimal;
+
+@Entity
+public class Product {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    //rond het getal op 2 decimalen af
+    @DecimalMin(value = "0.01")
+    private BigDecimal price;
+
+    public Product() {
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+}
+
